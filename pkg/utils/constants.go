b337392eda00973bfe81d6ebe129639bdaa3465c
ahmad-khatib0 <ahmadkhatib923@gmail.com>
2025-06-04 13:38:04 +0300

    utils constants

---
+++ pkg/utils/constants.go
@@ -0,0 +1,9 @@
+package utils
+
+const (
+	LowercaseLetters = "abcdefghijklmnopqrstuvwxyz"
+	UppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	Numbers          = "0123456789"
+	Symbols          = " !\"\\#$%&'()*+,-./:;<=>?@[]^_`|~"
+	MaxPropSizeBytes = 1024 * 1024
+)
