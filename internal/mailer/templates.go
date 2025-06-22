579719de146d202c49a1fad0042302d8f80c38b2
ahmad-khatib0 <ahmadkhatib923@gmail.com>
2025-06-22 13:40:52 +0300

    add templates helpers to mailer pkg

---
+++ internal/mailer/templates.go
@@ -0,0 +1,139 @@
+package mailer
+
+import (
+	"bytes"
+	"html/template"
+	"io"
+	"path/filepath"
+	"sync"
+
+	"github.com/fsnotify/fsnotify"
+)
+
+// TemplateContainer represents a set of templates that can be render
+type TemplateContainer struct {
+	templates *template.Template
+	mutex     sync.RWMutex
+	stop      chan struct{}
+	stopped   chan struct{}
+	watch     bool
+}
+
+// Data contains the data used to populate the template variables, it has Props
+// that can be of any type and HTML that only can be `template.HTML` types.
+type TemplateData struct {
+	Props map[string]any
+	Html  map[string]template.HTML
+}
+
+func NewTemplateContainerFromTemplate(t *template.Template) *TemplateContainer {
+	return &TemplateContainer{templates: t}
+}
+
+// NewTemplateContainer creates a new templates container
+func NewTemplateContainer(dir string) (*TemplateContainer, error) {
+	c := &TemplateContainer{}
+
+	htmlTemplates, err := template.ParseGlob(filepath.Join(dir, "*.html"))
+	if err != nil {
+		return nil, err
+	}
+
+	c.templates = htmlTemplates
+	return c, nil
+}
+
+// NewTemplateContainerWatcher creates a new templates container scanning a directory and
+// watch the directory filesystem changes to apply them to the loaded
+// templates. This function returns the container and an errors channel to pass
+// all errors that can happen during the watch process, or an regular error if
+// we fail to create the templates or the watcher. The caller must consume the
+// returned errors channel to ensure not blocking the watch process.
+func NewTemplateContainerWatcher(dir string) (*TemplateContainer, <-chan error, error) {
+	htmlTemplates, err := template.ParseGlob(filepath.Join(dir, "*.html"))
+	if err != nil {
+		return nil, nil, err
+	}
+
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	err = watcher.Add(dir)
+	if err != nil {
+		watcher.Close()
+		return nil, nil, err
+	}
+
+	c := &TemplateContainer{
+		templates: htmlTemplates,
+		watch:     true,
+		stop:      make(chan struct{}),
+		stopped:   make(chan struct{}),
+	}
+	errors := make(chan error)
+
+	go func() {
+		defer close(errors)
+		defer close(c.stopped)
+		defer watcher.Close()
+
+		for {
+			select {
+			case <-c.stop:
+				return
+			case event := <-watcher.Events:
+				if event.Op&fsnotify.Write == fsnotify.Write {
+					if htmlTemplates, err := template.ParseGlob(filepath.Join(dir, "*.html")); err != nil {
+						errors <- err
+					} else {
+						c.mutex.Lock()
+						c.templates = htmlTemplates
+						c.mutex.Unlock()
+					}
+				}
+			case err := <-watcher.Errors:
+				errors <- err
+			}
+		}
+	}()
+
+	return c, errors, nil
+}
+
+// Close stops the templates watcher of the container in case you have created
+// it with watch parameter set to true
+func (c *TemplateContainer) Close() {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+	if c.watch {
+		close(c.stop)
+		<-c.stopped
+	}
+}
+
+// RenderToString renders the template referenced with the template name using
+// the data provided and return a string with the result
+func (c *TemplateContainer) RenderToString(templateName string, data TemplateData) (string, error) {
+	var text bytes.Buffer
+	if err := c.Render(&text, templateName, data); err != nil {
+		return "", err
+	}
+
+	return text.String(), nil
+}
+
+// Render renders the template referenced with the template name using
+// the data provided and write it to the writer provided
+func (c *TemplateContainer) Render(w io.Writer, templateName string, data TemplateData) error {
+	c.mutex.Lock()
+	ht := c.templates
+	c.mutex.Unlock()
+
+	if err := ht.ExecuteTemplate(w, templateName, data); err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ internal/mailer/templates_test.go
@@ -0,0 +1,104 @@
+package mailer
+
+import (
+	"bytes"
+	"html/template"
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestHtmlTemplateWatcher(t *testing.T) {
+	dir, err := os.MkdirTemp("", "")
+	require.NoError(t, err)
+	defer os.RemoveAll(dir)
+
+	require.NoError(t, os.Mkdir(filepath.Join(dir, "templates"), 0o700))
+	require.NoError(t, os.WriteFile(filepath.Join(dir, "templates", "foo.html"), []byte(`{{ define "foo" }}foo{{ end }}`), 0o600))
+
+	preDir, err := os.Getwd()
+	require.NoError(t, err)
+	defer os.Chdir(preDir)
+	os.Chdir(dir)
+
+	watcher, errCh, err := NewTemplateContainerWatcher("templates")
+	require.NoError(t, err)
+	require.NotNil(t, watcher)
+	select {
+	case msg := <-errCh:
+		err = msg
+	default:
+		err = nil
+	}
+
+	require.NoError(t, err)
+	defer watcher.Close()
+
+	text, err := watcher.RenderToString("foo", TemplateData{})
+	require.NoError(t, err)
+	assert.Equal(t, "foo", text)
+	require.NoError(t, os.WriteFile(filepath.Join(dir, "templates", "foo.html"), []byte(`{{ define "foo" }}bar{{ end }}`), 0o600))
+
+	require.Eventually(t, func() bool {
+		text, err := watcher.RenderToString("foo", TemplateData{})
+		return text == "bar" && err == nil
+	}, time.Millisecond*100, time.Millisecond*50)
+}
+
+func TestNewTemplateContainerWatcher_BadDirectory(t *testing.T) {
+	watcher, errChan, err := NewTemplateContainerWatcher("not_exists")
+	require.Error(t, err)
+	assert.Nil(t, watcher)
+	assert.Nil(t, errChan)
+}
+
+func TestRender(t *testing.T) {
+	tpl := template.New("test")
+	_, err := tpl.Parse(`{{ define "foo" }}foo{{ .Props.Bar }}{{ end }}`)
+	require.NoError(t, err)
+
+	mt := NewTemplateContainerFromTemplate(tpl)
+	data := TemplateData{
+		Props: map[string]any{"Bar": "bar"},
+	}
+
+	text, err := mt.RenderToString("foo", data)
+	require.NoError(t, err)
+	assert.Equal(t, "foobar", text)
+
+	buf := &bytes.Buffer{}
+	require.NoError(t, mt.Render(buf, "foo", data))
+	assert.Equal(t, "foobar", buf.String())
+}
+
+func TestRenderError(t *testing.T) {
+	tpl := template.New("test")
+	_, err := tpl.Parse(`{{ define "foo" }}foo{{ .Foo.Bar }}bar{{ end }}`)
+	require.NoError(t, err)
+
+	mt := NewTemplateContainerFromTemplate(tpl)
+	text, err := mt.RenderToString("foo", TemplateData{})
+	require.Error(t, err)
+	assert.Equal(t, "", text)
+
+	buf := bytes.Buffer{}
+	assert.Error(t, mt.Render(&buf, "foo", TemplateData{}))
+	assert.Equal(t, "foo", buf.String())
+}
+
+func TestRenderUnknownTemplate(t *testing.T) {
+	tpl := template.New("")
+	mt := NewTemplateContainerFromTemplate(tpl)
+
+	text, err := mt.RenderToString("foo", TemplateData{})
+	require.Error(t, err)
+	assert.Equal(t, "", text)
+
+	buf := &bytes.Buffer{}
+	assert.Error(t, mt.Render(buf, "foo", TemplateData{}))
+	assert.Equal(t, "", buf.String())
+}
